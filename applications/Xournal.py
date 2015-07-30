b5b41cca172d8dc74f9ea5d8032da30040b41a99
RobbieNor <mail@rjnorman.co.uk>
2015-07-30 18:14:05 +0100

    First Commit

---
+++ applications/Xournal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+__extension__ = ".xoj"
+
+
+def createProgramLine(path):
+	return "xournal '" + path + "'"
+
+def getExtension():
+	return __extension__
+	
+	
---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xournal '/home/robbienor/.note_organiser/testdir/Level 4/Atoms, Lasers and Qubits/Laser Physics/Weekly Problem/Weekly Problem 01 - LP.1.xoj'
