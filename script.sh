2f95d3a2dc7385a2cc2b58b868610a4b5ae46545
RobbieNor <mail@rjnorman.co.uk>
2015-08-01 00:49:50 +0100

    Ensure brac updated

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xournal '/home/robbienor/.note_organiser/testdir/Level 4/Atoms, Lasers and Qubits/Laser Physics/Weekly Problem/Weekly Problem 01 - LP.1.xoj'
--- workspace/script.sh
+++ workspace/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xournal '/home/robbienor/Documents/Programs/Git Repositories/lecture-note-creator/testing/nbk/Level 1/Foundations of Physics 1/Classical Mechanics/Lecture/Lecture 07 - Testing 7.xoj'
+xournal '/home/robbienor/Documents/Programs/Git Repositories/lecture-note-creator/testing/nbk/Level 1/Foundations of Physics 1/Classical Mechanics/Lecture/Lecture 03 - Testing 2.xoj'
